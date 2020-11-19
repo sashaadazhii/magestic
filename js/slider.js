7e59dc665d00806bfac010c96e628356d815c0ab
sashaadazhii <sashki.sashka@gmail.com>
2020-11-19 12:24:29 +0200

    slider with swiper

--- js/slider.js
+++ js/slider.js
@@ -1,43 +1,56 @@
-let div = document.getElementsByClassName('img-wrapper')
+// let div = document.getElementsByClassName('img-wrapper')
 
-let timer
-let slideIndex = 1
+// let timer
+// let slideIndex = 1
 
-let deviceWidth = document.body.clientWidth
+// let deviceWidth = document.body.clientWidth
 
-showSlides(slideIndex)
+// showSlides(slideIndex)
 
-function plusSlides(n) {
-  showSlides((slideIndex += n))
-}
+// function plusSlides(n) {
+//   showSlides((slideIndex += n))
+// }
 
-function currentSlide(n) {
-  showSlides((slideIndex = n))
-}
+// function currentSlide(n) {
+//   showSlides((slideIndex = n))
+// }
 
-function showSlides(n) {
-  let i
-  let slides = document.getElementsByClassName('slider__item')
-  let dots = document.getElementsByClassName('slider-dots__item')
-  if (n > slides.length) {
-    slideIndex = 1
+// function showSlides(n) {
+//   let i
+//   let slides = document.getElementsByClassName('slider__item')
+//   let dots = document.getElementsByClassName('slider-dots__item')
+//   if (n > slides.length) {
+//     slideIndex = 1
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = 'none'
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(' active', '')
+//   }
+//   slides[slideIndex - 1].style.display = 'block'
+//   dots[slideIndex - 1].className += ' active'
+
+//   if (deviceWidth <= 1024) {
+//     slides[slideIndex - 1].style.display = 'flex'
+//   }
+
+//   // clearTimeout(timer)
+//   // timer = setTimeout(() => plusSlides(1), 3000)
+// }
+
+var swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  // autoplay: {
+  //   delay: 1500,
+  //   disableOnInteraction: false
+  // },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
   }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none'
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '')
-  }
-  slides[slideIndex - 1].style.display = 'block'
-  dots[slideIndex - 1].className += ' active'
-
-  if (deviceWidth <= 1024) {
-    slides[slideIndex - 1].style.display = 'flex'
-  }
-
-  // clearTimeout(timer)
-  // timer = setTimeout(() => plusSlides(1), 3000)
-}
+})
